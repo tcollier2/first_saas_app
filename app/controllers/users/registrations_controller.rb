a8e3f9fced9666643e96f13d5e8dd88987929c63
Tony Collier <tcollier2@gmail.com>
2015-07-27 14:41:29 +0000

    Changes for Stripe api

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
    before_filter :select_plan, only: :new
    
    
-   def create
+   def create 
       super do |resource|
          if params[:plan]
              resource.plan_id = params[:plan]
