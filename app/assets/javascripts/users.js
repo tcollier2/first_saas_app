ebecd6e3a3ad3583b8ef644ef772a19e1131e22a
Tony Collier <tcollier2@gmail.com>
2015-07-28 14:32:55 +0000

    Card to Source overhaul for stripe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum = $('.card_number').val(),
-        cvcNum = $('.card_code').val(),
-        expMonth = $('#card_month').val(),
-        expYear = $('#card_year').val();
+    var ccNum = $('#source_number').val(),
+        cvcNum = $('#source_code').val(),
+        expMonth = $('#source_month').val(),
+        expYear = $('#source_year').val();
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -26,7 +26,7 @@ $(document).ready(function() {
     // Get the token from the response:
     var token = response.id;
     // Add the token to the form:
-    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+    f.append('<input type="hidden" name="user[stripe_source_token]" value="' + token + '" />');
     // Submit the form:
     f.get(0).submit(); 
   }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
-  attr_accessor :stripe_card_token
+  attr_accessor :stripe_source_token
   
   def save_with_payment
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_source_token)
       self.stripe_customer_token = customer.id
       save! 
     end
