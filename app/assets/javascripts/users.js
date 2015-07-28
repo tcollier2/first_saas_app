9c6850acff03994c67aaacf64efbb4e8465118ed
Tony Collier <tcollier2@gmail.com>
2015-07-28 16:24:33 +0000

    Overhaul to Rob's code for Stripe success

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum = $('#source_number').val(),
-        cvcNum = $('#source_code').val(),
-        expMonth = $('#source_month').val(),
-        expYear = $('#source_year').val();
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -20,13 +21,17 @@ $(document).ready(function() {
     }
     return false;
   }); // form submission
+  
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
-    var f = $(".new_user");
+    var f = $("#new_user");
+
     // Get the token from the response:
     var token = response.id;
+
     // Add the token to the form:
-    f.append('<input type="hidden" name="user[stripe_source_token]" value="' + token + '" />');
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
     // Submit the form:
     f.get(0).submit(); 
   }
